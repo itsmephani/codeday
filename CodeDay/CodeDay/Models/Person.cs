702f84855df96c80b9606e953aba0e516d7480af
unknown <hack2hire08@hack2hire.net>
2019-04-07 12:16:53 +0530

    codedady .net learning. first commit.

---
+++ CodeDay/CodeDay/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CodeDay.Models
+{
+  class Person : INotifyPropertyChanged
+  {
+    public Person()
+    {
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+  }
+}
